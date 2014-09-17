03d639149fc6ff726a5a0e73918bc051274790d3
Rufr <ANTHONY@anthonys-MacBook-Air.local>
2014-09-17 19:34:19 -0400

    V1.01
    Array problems

---
+++ GraphicTextRpg/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GraphicTextRpg
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Array Map;
+			Array col;
+			Array row;
+
+			int x=0;
+			int y=0;
+			while (true) 
+			{
+				add(row, "_");
+				x += 1;
+				if (x>=50)
+				{
+					x=0;
+					y+=1;
+					Array.add(col, row);
+					if (y>=50)
+					{
+						Map=col;
+						break;
+					}
+				}
+			}
+				
+		}
+	}
+}
