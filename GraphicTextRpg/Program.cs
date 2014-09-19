bc3e50c53c15e9a382e5a2e75bd14431d22ce1be
Rufr <ANTHONY@anthonys-MacBook-Air.local>
2014-09-18 20:57:01 -0400

    Added small plot…ish

--- GraphicTextRpg/Program.cs
+++ GraphicTextRpg/Program.cs
@@ -6,29 +6,77 @@ namespace GraphicTextRpg
 	{
 		public static void Main (string[] args)
 		{
-			Array Map;
-			Array col;
-			Array row;
+			string location = "Menu";string responce;string ocupation;string past_location="none";
 
-			int x=0;
-			int y=0;
-			while (true) 
+
+
+			switch (location)
 			{
-				add(row, "_");
-				x += 1;
-				if (x>=50)
-				{
-					x=0;
-					y+=1;
-					Array.add(col, row);
-					if (y>=50)
-					{
-						Map=col;
-						break;
-					}
+
+			case "Menu":
+				System.Console.WriteLine ("Welcome to a quick adventure game I made");
+				System.Console.WriteLine ("Press RETURN to start");
+				System.Console.ReadLine ();
+				goto case "Character Setup";
+
+
+			case "Character Setup":
+				System.Console.Write ("Name your player:");
+				string Player_Name = System.Console.ReadLine ();
+				System.Console.WriteLine ("Well hello " + Player_Name + " I haven\'t met you before, what do you do?\n[a]\tTrader\n[b]\tHunter\n[c]\tFarmer");
+				responce = System.Console.ReadLine ();
+				if (responce == "a")
+					ocupation = "traider";
+				else if (responce == "b")
+					ocupation = "hunter";
+				else if (responce == "c")
+					ocupation = "farmer";
+				else
+					ocupation = "traider";
+
+				System.Console.WriteLine("You are now in main game, try typing things and figuring out what to do!\nonly letters please\nalso use commands like up, rather than get up\n\n:D\n");
+				goto case "register responce";
+
+			
+			case "register responce":
+				responce = System.Console.ReadLine ().ToLower ();
+
+				if (responce == "help") {
+					System.Console.WriteLine ("Try to figure it out on your own");
+					goto case "register responce";
 				}
+
+				if (responce == "hi" || responce == "hello") {
+					System.Console.WriteLine ("<From the darkness> Hello...");
+					goto case "register responce";
+				}
+
+				if (responce == "who are you") {
+					System.Console.WriteLine ("<From the darkness> I am a monster...so people say...");
+					goto case "register responce";
+				}
+
+				if (responce == "up") {
+					System.Console.WriteLine ("Chains rattle at your feet...Can i find a key?");
+					goto case "register responce";
+				}
+
+				if (responce == "look") {
+					System.Console.WriteLine ("<From the darkness> Are you looking for a key...here");
+					goto case "register responce";
+				}
+
+
+				break;
+
+
+
 			}
-				
+
+
+
+
+
 		}
 	}
 }
